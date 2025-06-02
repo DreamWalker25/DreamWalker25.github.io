ea9f926829faa863fd5cb056a9e8b97826029133
DreamWalker25 <pawlakpiotr2411@gmail.com>
2025-06-02 22:21:52 +0200

    plik php i style

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikacja pogodowa</title>
+    <link rel='stylesheet' href='style.scss'>
+    
+</head>
+<body>
+    <h1>Witaj w aplikacji pogodowej!</h1>
+    <?php echo "chuj<br/>"; 
+            echo "Aktualna data i czas: " . date("Y-m-d H:i:s");
+    ?>
+</body>
+</html>
