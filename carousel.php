f36e161cee756abcee2cd196a49d00e4b2d0000c
DreamWalker25 <pawlakpiotr2411@gmail.com>
2025-06-03 11:08:09 +0200

    edycja napisu na karuzeli, jest zawsze na wierzchu

--- carousel.php
+++ carousel.php
@@ -1,14 +1,11 @@
-<header>
+<header class="position-relative">
+  <div class="postion-absolute carousel-caption d-flex h-100 align-items-center justify-content-center" style="z-index: 10;">
+      <h1 class="display-3 text-white bg-dark bg-opacity-50 p-3 rounded">Pogodynka</h1>
+  </div>
   <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-caption position-absolute z d-flex h-100 align-items-center justify-content-center">
-          <h1 class="display-3 text-white bg-dark bg-opacity-50 p-3 rounded">Pogodynka</h1>
-        </div>
       <div class="carousel-item active">
         <img src="img/sloneczna.jpg" class="d-block w-100" alt="Pogoda słoneczna">
-        <div class="carousel-caption d-flex h-100 align-items-center justify-content-center">
-          <h1 class="display-3 text-white bg-dark bg-opacity-50 p-3 rounded">Pogodynka</h1>
-        </div>
       </div>
       <div class="carousel-item">
         <img src="img/pochmurna.jpg" class="d-block w-100" alt="Zachmurzenie">
