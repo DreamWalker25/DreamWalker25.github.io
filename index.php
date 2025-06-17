a8ad5f25bf452ddb70ade3087bbb7d68b5a98763
DreamWalker25 <pawlakpiotr2411@gmail.com>
2025-06-17 20:09:25 +0200

    Wyczyszczenie kodu źródłowego

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 $city = isset($_POST['city']) ? trim($_POST['city']) : 'Gliwice';
-include 'weather.php';
-include 'forecast.php';
-include 'config.php';
+
+
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
